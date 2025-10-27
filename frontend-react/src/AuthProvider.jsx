70749527fea8706ae52f995d72cc313affd03929
dark-lord44327 <chan17der@gmail.com>
2025-10-27 23:53:32 +0530

    login functionality

---
+++ frontend-react/src/AuthProvider.jsx
@@ -0,0 +1,18 @@
+import React, { createContext } from 'react'
+import { useState , useContext } from 'react'
+
+const AuthContext = createContext();
+
+const AuthProvider = ({children}) => {
+  const [isLoggedIn , setIsLoggedIn] = useState(
+     !!localStorage.getItem('accessToken')
+  )
+  return (
+    <AuthContext.Provider value ={{isLoggedIn , setIsLoggedIn}}>
+            {children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
+export {AuthContext}
